4534862499389c3ea44725bc1b1f1e886bcd768a
Ian <ian@fruitstreet.com>
2020-06-25 19:31:20 -0700

    Add enemy wave plumbing (but not for random waves yet.)

--- SpaceEncoders/GameScene.swift
+++ SpaceEncoders/GameScene.swift
@@ -25,7 +25,13 @@ class GameScene: SKScene {
 //    let shipInfo = Bundle.main.decode([ShipInfo].self, from: "ships")
     let waves = Bundle.main.decode([Wave].self, from: "waves")
     let enemyTypes = Bundle.main.decode([EnemyType].self, from: "enemy-types")
+    
+    var isPlayerAlive = true
+    var levelNumber = 0
+    var waveNumber = 0
 
+    let positions = Array(stride(from: -320, through: 320, by: 80))
+    
     override func didMove(to view: SKView) {
         if let particles = SKEmitterNode(fileNamed: "Starfield") {
             particles.position = CGPoint(x: 0, y: frame.height)
@@ -33,16 +39,37 @@ class GameScene: SKScene {
             particles.advanceSimulationTime(60)
             addChild(particles)
             
-            let ship = MakeShip(type: "ship1")
+            let ship = makeShip(type: "ship1")
             ship.position = CGPoint(x: 0, y: frame.minY + 100)
             ship.zPosition = 1
             addChild(ship)
         }
     }
     
-    func MakeShip(type: String) -> Ship {
+    func makeShip(type: String) -> Ship {
         return Ship(imageNamed: type)
     }
 
-    
+    func createWave() {
+        guard isPlayerAlive else { return }
+        
+        if waveNumber == waves.count {
+            levelNumber += 1
+            waveNumber = 0
+        }
+        
+        let currentWave = waves[waveNumber]
+        waveNumber += 1
+        
+        let maximumEnemyType = min(enemyTypes.count, levelNumber + 1)
+        let enemyType = Int.random(in: 0..<maximumEnemyType)
+        let enemyOffset:CGFloat = 100
+        let enemyStartOffset = 600
+        
+        for enemy in currentWave.enemies {
+            let node = EnemyNode(type: enemyTypes[enemyType], startPosition: CGPoint(x: positions[enemy.position], y: enemyStartOffset), offset: enemy.offset, moveType: enemy.moveStyle)
+            addChild(node)
+        }
+        
+    }
 }
